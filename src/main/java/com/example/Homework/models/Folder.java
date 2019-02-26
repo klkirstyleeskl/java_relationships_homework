4d0342589ed7f1d147e5603f6f295acd469e5dd5
klkirstyleeskl <k.lees@btinternet.com>
2019-02-26 16:21:44 +0000

    fleshes out folders class

--- src/main/java/com/example/Homework/models/Folder.java
+++ src/main/java/com/example/Homework/models/Folder.java
@@ -1,4 +1,59 @@
 package com.example.Homework.models;
 
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
+
+
 public class Folder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    @Column(name = "title")
+    private String tile;
+
+
+
+    private List<File> files;
+
+
+    public Folder(String tile, List<File> files) {
+        this.tile = tile;
+        this.files = new ArrayList<>();
+    }
+
+    public Folder(){
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTile() {
+        return tile;
+    }
+
+    public void setTile(String tile) {
+        this.tile = tile;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
 }
